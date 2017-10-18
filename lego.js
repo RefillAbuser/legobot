c6778be59b01f6e96de59ff357d746fdafc82712
darkend1 <31580770+RefillAbuser@users.noreply.github.com>
2017-10-18 10:03:33 -0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -1,11 +1,20 @@
 const Discord = require('discord.js');
 const lego = new Discord.Client();
+const config = require("./config.json");
 
 lego.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 bot.user.setPresence({game: {name: 'a bot that gives no fucks', type:0 } });
+// Prefix settings
+bot.on('message', message => {
+ if(message.author.bot) return;
+ if(!message.content.startsWith(config.prefix)) return;
+
+ let command = message.content.split(" ")[0];
+ command = command.slice(config.prefix.length);
+ let args = message.content.split(" ").slice(1);
 
 lego.on('message', message => {
   if (message.content === 'ping') {
