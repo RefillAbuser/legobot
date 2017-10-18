bc11ed3e19cbe6ac2f4b10ea89fdf0d84eca9729
darkend1 <31580770+RefillAbuser@users.noreply.github.com>
2017-10-18 08:52:26 -0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -1,14 +1,20 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.login(process.env.BOT_TOKEN);
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', message => {
+  if (!message.guild) return;
+
+  if (message.content === '+join') {
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => {
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
   }
 });
-
-client.login('token');
